df2c001e02cc5e4c23ad91e827f28cafe49ad6ce
Paul Murray <pawlmurray@gmail.com>
2011-10-18 17:21:39 -0400

    Starting rewrite rules

---
+++ src/main/scala/com/cpb/cs4500/io/fileInputOutput.scala
@@ -0,0 +1,19 @@
+package com.cpb.cs4500.io 
+{
+    import scala.io._
+    object fileInputOutput
+    {
+      def inputFromFile(fileName: String)
+      { 
+        val s = Source.fromFile(fileName)
+        s.getLines.foreach( (line) => {println(line.trim.toUpperCase)})
+      }
+      def outputToFile(fileName: String, fileContent: String)
+      {
+        val out = new java.io.FileWriter(fileName)
+        out.write(fileContent)
+        out.close
+      }
+    }
+    
+}
---
+++ src/main/scala/com/cpb/cs4500/rewriting/Rewriter.scala
@@ -0,0 +1,5 @@
+package com.cpb.cs4500.rewriting {
+    import com.cpb.cs4500.parsing._
+    class Rewriter(specification:Spec) {
+    }
+}
