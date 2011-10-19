7a1ac954377376f83ee08909a7015b0a1e292764
Paul Murray <pawlmurray@gmail.com>
2011-10-18 21:14:26 -0400

    final commit maybe?

--- src/main/scala/com/cpb/cs4500/io/ReadWriter.scala
+++ src/main/scala/com/cpb/cs4500/io/ReadWriter.scala
@@ -5,9 +5,10 @@ package com.cpb.cs4500.io
     {
       def inputFromFile(fileName: String):String = 
       { 
-        val s = Source.fromFile(fileName)
-        val str:String = ""
-        s.getLines.foreach((line)=> {str+line})
+      
+        var str:String = ""
+        for (line <- Source.fromFile(fileName).getLines)
+            str+= " " + line + " "
         str 
       }
       def outputToFile(fileName: String, fileContent: String):Unit = 
@@ -15,11 +16,6 @@ package com.cpb.cs4500.io
         val out = new java.io.FileWriter(fileName)
         out.write(fileContent)
         out.close
-      }
-      
-      def durp()
-      {
-        println("HEYEHYEYEHEY")
       }
     }
     
--- src/test/scala/com/cpb/cs4500/rewriting/TestRewriter.scala
+++ src/test/scala/com/cpb/cs4500/rewriting/TestRewriter.scala
@@ -3,6 +3,7 @@ package com.cpb.cs4500.rewriting {
   import org.scalatest.FunSuite
   import com.cpb.cs4500.rewriting._
   import com.cpb.cs4500.parsing._
+  import com.cpb.cs4500.io._
 
   class TestRewriter extends FunSuite {
     val testPhraseYay:String =  "Signatures: ADT: THISADT makeBool: int * int -> boolean " +
@@ -13,6 +14,9 @@ package com.cpb.cs4500.rewriting {
    
     val parser = new ADTParser()
     val rewriter:Rewriter = new Rewriter()
+    val input:String = ReadWriter.inputFromFile("C:\\Users\\Paul\\Documents\\School\\Fall 2011\\Software Dev\\cs4500\\src\\main\\scala\\com\\cpb\\cs4500\\io\\testInput.txt")
+    println("INPUT: " + input)
+    ReadWriter.outputToFile("meow.txt", rewriter.applyRewriteRules(parser.parseAll(parser.spec, input).get))
     test("RewriteTest1") {expect("ADT named: THISADT" + "\n" + "(test (makeBool int int) boolean)") 
                          {rewriter.applyRewriteRules(parser.parseAll(parser.spec, testPhraseYay).get)}}
   }
