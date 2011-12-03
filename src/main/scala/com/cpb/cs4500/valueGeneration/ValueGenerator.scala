5a61b58953a60228523b2516a34b52611be80f94
Paul Murray <pawlmurray@gmail.com>
2011-12-03 18:27:51 -0500

    updated ValueGenerator to handle primitives

--- src/main/scala/com/cpb/cs4500/valueGeneration/ValueGenerator.scala
+++ src/main/scala/com/cpb/cs4500/valueGeneration/ValueGenerator.scala
@@ -3,7 +3,7 @@ package com.cpb.cs4500.valueGeneration {
     import com.cpb.cs4500.parsing._
     import com.cpb.cs4500.rewriting._
     import com.cpb.cs4500.io._
-    import scala.collection.mutable.HashMap
+    import scala.collection.mutable._
     import scala.util._
     
 
@@ -14,9 +14,43 @@ package com.cpb.cs4500.valueGeneration {
             System.out.println("HNNNNNG")
         }
         
-        def generatedFunctionValues(opspec:OperationSpec) : List[GeneratedValue] =
+        def generatedFunctionValues(opspec:OperationSpec) : GeneratedFunction =
         {
-            null
+            val operationName:String = opspec.getOpName
+            val returnLiteral:Terminal = opspec.returnType
+            val arguments:List[Terminal] = opspec.argTypes.args
+            var listGenValues:List[GeneratedValue] = List[GeneratedValue]()
+            for (arg<-arguments)
+            {
+                arg match{
+                    case intLit:IntLiteral => listGenValues = listGenValues :+ (new GeneratedPrimitive(generateRandomIntLiteral()))
+                    case boolLit:BooleanLiteral => listGenValues = listGenValues :+ (new GeneratedPrimitive(generateRandomBooleanLiteral()))
+                    case charLit:CharLiteral => listGenValues = listGenValues :+ (new GeneratedPrimitive(generateRandomCharLiteral()))
+                    case stringLit:StringLiteral => listGenValues = listGenValues :+ (new GeneratedPrimitive(generateRandomStringLiteral()))
+                }
+            }
+            new GeneratedFunction(operationName, listGenValues, returnLiteral)
+        }
+        
+        def generateRandomIntLiteral():IntLiteral= 
+        {
+            IntLiteral(scala.util.Random.nextInt(1001).toString)
+        }
+      
+        def generateRandomBooleanLiteral():BooleanLiteral=
+        {
+            BooleanLiteral(scala.util.Random.nextBoolean.toString)
+        }
+      
+        def generateRandomCharLiteral():CharLiteral=
+        {
+            CharLiteral(scala.util.Random.nextPrintableChar.toString)
+        }
+      
+        def generateRandomStringLiteral():StringLiteral=
+        {
+            val strList = List("string1", "string2", "string3", "string4", "string5")
+            StringLiteral(strList(scala.util.Random.nextInt(5)))
         }
     }
 }
