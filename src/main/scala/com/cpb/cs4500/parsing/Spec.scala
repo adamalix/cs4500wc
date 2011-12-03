aaad78a2ef04c2dfcfb19a3f22e4b307c1176d4b
Adam Alix <adam.alix@gmail.com>
2011-12-03 14:51:01 -0500

    homework 6 tagged

--- src/main/scala/com/cpb/cs4500/parsing/Spec.scala
+++ src/main/scala/com/cpb/cs4500/parsing/Spec.scala
@@ -8,38 +8,37 @@
 package com.cpb.cs4500.parsing {
   import scala.collection.mutable.HashMap
   import scala.collection.immutable._
-  
-  
+
+
   trait Terminal {
     override def toString():String
   }
 
-  case class Spec(signatures:ADTSignatures, equations:Equations) 
-  {
+  case class Spec(signatures:ADTSignatures, equations:Equations) {
     override def toString():String = {
       signatures.toSexpr
     }
-    
+
     def toGeneratedSexpr(repMap:HashMap[OperationSpec, List[((ArgTypes, TypeLiteral))]]):String = {
         signatures.toGeneratedSexpr(repMap)
     }
-    
+
     def getAllTypeNames():ListSet[TypeName] = {
         signatures.getAllTypeNames()
     }
-    
+
     def getAllTypeLiterals():ListSet[TypeLiteral] = {
         signatures.getAllTypeLiterals()
     }
-    
+
     def getAllTypes():ListSet[Terminal] = {
         signatures.getAllTypeNames() ++ signatures.getAllTypeLiterals()
     }
-    
+
     def getAllOpSpecs():ListSet[OperationSpec] = {
         signatures.getAllOpSpecs
     }
-    
+
     def getAllBaseConstructors():ListSet[OperationSpec] = {
         var ops:ListSet[OperationSpec] = getAllOpSpecs
         var cons:ListSet[OperationSpec] = ListSet[OperationSpec]()
@@ -52,49 +51,48 @@ package com.cpb.cs4500.parsing {
         }
         cons
     }
-    
+
     def getOperationNames():List[String] = {
         val ops:ListSet[OperationSpec] = getAllOpSpecs
         var opNameList:List[String] = List[String]()
-        
+
         for (op<-ops){
             opNameList = opNameList :+ op.getOpName
         }
         opNameList
-        
+
     }
   }
 
-  case class ADTSignatures(sigs:List[ADTSignature])  
-  {
+  case class ADTSignatures(sigs:List[ADTSignature]) {
     def toSexpr():String = {
       var sexpr:String = ""
       sigs.foreach((sig:ADTSignature) => sexpr+=sig.toSexpr)
       sexpr
     }
-    
+
     def toGeneratedSexpr(repMap:HashMap[OperationSpec, List[((ArgTypes, TypeLiteral))]]):String = {
       var sexpr:String = ""
       sigs.foreach((sig:ADTSignature) => sexpr+=sig.toGeneratedSexpr(repMap))
       sexpr
     }
-    
+
     def getAllTypeNames():ListSet[TypeName] = {
         var allTypeNames:ListSet[TypeName] = new ListSet[TypeName]()
         for (sig<-sigs){
             allTypeNames = allTypeNames ++ sig.getAllTypeNames
         }
-        allTypeNames    
+        allTypeNames
     }
-    
+
     def getAllTypeLiterals():ListSet[TypeLiteral] = {
         var allTypeLits:ListSet[TypeLiteral] = new ListSet[TypeLiteral]()
         for (sig<-sigs){
             allTypeLits = allTypeLits ++ sig.getAllTypeLiterals
         }
-        allTypeLits   
+        allTypeLits
     }
-    
+
     def getAllOpSpecs():ListSet[OperationSpec] = {
         var AllOpSpecs:ListSet[OperationSpec] = new ListSet[OperationSpec]()
         for (sig<-sigs){
@@ -103,28 +101,28 @@ package com.cpb.cs4500.parsing {
         AllOpSpecs
     }
   }
-  
+
   case class ADTSignature(name:TypeName, opSpecs:OperationSpecs) {
     def toSexpr():String = {
       opSpecs.toSexpr
     }
-    
+
     def toGeneratedSexpr(repMap:HashMap[OperationSpec, List[((ArgTypes, TypeLiteral))]]):String = {
         opSpecs.toGeneratedSexpr(repMap)
     }
-    
+
     def getAllTypeNames():ListSet[TypeName] = {
       var allTypeNames:ListSet[TypeName] = new ListSet[TypeName]()
       allTypeNames = allTypeNames + name ++ opSpecs.getAllTypeNames
       allTypeNames
     }
-    
+
     def getAllTypeLiterals():ListSet[TypeLiteral] = {
       var allTypeLits:ListSet[TypeLiteral] = new ListSet[TypeLiteral]()
       allTypeLits = allTypeLits ++ opSpecs.getAllTypeLiterals
       allTypeLits
     }
-    
+
     def getAllOpSpecs():List[OperationSpec] = {
         opSpecs.getOpSpecs
     }
@@ -136,13 +134,13 @@ package com.cpb.cs4500.parsing {
       ops.foreach((op:OperationSpec) => sexpr+= op.toSexpr + "\n")
       sexpr
     }
-    
+
     def toGeneratedSexpr(repMap:HashMap[OperationSpec, List[((ArgTypes, TypeLiteral))]]):String = {
       var sexpr:String = ""
       ops.foreach((op:OperationSpec) => sexpr+= op.toGeneratedSexpr(repMap) + "\n")
       sexpr
     }
-    
+
     def getAllTypeNames():ListSet[TypeName] = {
       var allTypeNames:ListSet[TypeName] = new ListSet[TypeName]()
       for(op<-ops){
@@ -150,6 +148,7 @@ package com.cpb.cs4500.parsing {
       }
       allTypeNames
     }
+
     def getAllTypeLiterals():ListSet[TypeLiteral] = {
       var allTypeLits:ListSet[TypeLiteral] = new ListSet[TypeLiteral]()
       for(op<-ops){
@@ -157,11 +156,11 @@ package com.cpb.cs4500.parsing {
       }
       allTypeLits
     }
-    
+
     def getOpSpecs():List[OperationSpec] = {
         ops
     }
-    
+
   }
 
   case class OperationSpec(op:Operation, argTypes:ArgTypes, returnType:Terminal, basicCreator:Boolean) {
@@ -169,13 +168,13 @@ package com.cpb.cs4500.parsing {
     def toSexpr():String = {
       "(test (" + op.toString + argTypes.toString + ") " + returnType.toString + ")"
     }
-    
+
     def toGeneratedSexpr(repMap:HashMap[OperationSpec, List[((ArgTypes, TypeLiteral))]]):String = {
       val newArgTypes:ArgTypes = repMap(this).head._1
       val newReturnType:Terminal = repMap(this).head._2
       "(test (" + op.toString + newArgTypes.toString + ") " + newReturnType.toString + ")"
     }
-    
+
     def getAllTypeNames():ListSet[TypeName] = {
         var allTypeNames:ListSet[TypeName] = new ListSet[TypeName]()
         returnType match {
@@ -186,18 +185,18 @@ package com.cpb.cs4500.parsing {
         allTypeNames = allTypeNames ++ argTypes.getAllTypeNames()
         allTypeNames
     }
-    
+
     def getAllTypeLiterals():ListSet[TypeLiteral] = {
         var allTypeLits:ListSet[TypeLiteral] = new ListSet[TypeLiteral]()
         returnType match {
             case a:TypeLiteral => allTypeLits = allTypeLits + a
-            case a:TypeName => 
+            case a:TypeName =>
             case a:Operation =>
         }
         allTypeLits = allTypeLits ++ argTypes.getAllTypeLiterals()
         allTypeLits
     }
-    
+
     def getOpName():String = {
         op.toString
     }
@@ -212,28 +211,28 @@ package com.cpb.cs4500.parsing {
     override def toString():String = ident
   }
 
-  case class ArgTypes(args:List[Terminal]){
+  case class ArgTypes(args:List[Terminal]) {
     override def toString():String = {
       var whole:String = ""
       args.foreach((arg:Terminal) => whole+= " " + arg.toString)
       whole
     }
-    
+
     def getAllTypeNames():ListSet[TypeName] = {
       var allTypeNames:ListSet[TypeName] = new ListSet[TypeName]()
       for (a<-args) a match {
         case a:TypeName => allTypeNames = allTypeNames + a
-        case a:TypeLiteral => 
+        case a:TypeLiteral =>
         case a:Operation =>
       }
       allTypeNames
     }
-        
+
     def getAllTypeLiterals():ListSet[TypeLiteral] = {
       var allTypeLits:ListSet[TypeLiteral] = new ListSet[TypeLiteral]()
         for (a<-args) a match {
             case a:TypeLiteral => allTypeLits = allTypeLits + a
-            case a:TypeName => 
+            case a:TypeName =>
             case a:Operation =>
         }
         allTypeLits
@@ -245,7 +244,7 @@ package com.cpb.cs4500.parsing {
   }
 
   case class TypeName(value:String) extends Terminal {
-  
+
     override def toString():String = value
   }
 
@@ -254,7 +253,7 @@ package com.cpb.cs4500.parsing {
   }
 
   case class Equation(left:Term, right:Term) {
-    
+
   }
 
   trait ArgTrait {
@@ -262,11 +261,11 @@ package com.cpb.cs4500.parsing {
   }
 
   case class Term(ident:String, op:Operation, args:ArgTrait) {
-    
+
   }
 
   case class Args(term:Term, args:ArgTrait) extends ArgTrait {
-    
+
   }
 
   // This is the empty case.
