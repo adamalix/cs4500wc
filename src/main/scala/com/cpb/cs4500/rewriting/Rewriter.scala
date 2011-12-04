87c944928746bf80995223f6c0c9824204ece11e
Phil Quinn <phil@Phil-Quinns-MacBook-Air.local>
2011-12-04 18:59:12 -0500

    added some functions to Rewriter.scala but they're just headers

--- src/main/scala/com/cpb/cs4500/rewriting/Rewriter.scala
+++ src/main/scala/com/cpb/cs4500/rewriting/Rewriter.scala
@@ -9,22 +9,26 @@ package com.cpb.cs4500.rewriting {
             "HNNGG"
         }
 
-       // def generateExpressions(specification:Spec):String = {
-         //   specification.signatures.sigs.foreach(
-       // }
+		def rewrite(values:List[GeneratedValue], rules:List[AnyRef]):List[GeneratedValue] = {
+            //for (value<-values) value match {
+            //    case value:GeneratedPrimitive => rewrite(value, rules)
+            //    case value:GeneratedFunction => {
+            //        List(applyRules(rules, value), 
+            //             rewrite(value.getArguments, rules))
+            //    }
+            //}
+            List()
+		}
 
-       // def generateValue(symbol:Terminal) symbol match {
-       //     case n: TypeName => "lol"
-       //     case l: TypeLiteral => generateLiteralValue(l)
-        //    case _ => "wrong again"
-       // }
-
-       // def generateLiteralValue(symbol:Terminal) = symbol.toString match {
-         //   case "int" => 0
-           // case "boolean" => true
-           // case "char" => 'a'
-           // case "string" => "hello"
-           // case _ => "unknown value"
-       // }
+        def applyRules(rules:List[AnyRef], value:GeneratedValue):GeneratedValue = {
+            val ruleApplies = true
+            for (rule<-rules) {
+                if (ruleApplies) {
+                    //rewrite the GeneratedValue, probably by making a new one
+                    value
+                }
+            }
+            value
+        }
     }
 }
