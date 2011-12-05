a82f1b3f58fa27d19bb78e37dc8981d315cf6172
Adam Alix <adam.alix@gmail.com>
2011-12-05 01:15:52 -0500

    added some stubs for rewriting and valgen

--- src/main/scala/com/cpb/cs4500/Runner.scala
+++ src/main/scala/com/cpb/cs4500/Runner.scala
@@ -3,24 +3,44 @@
  The main entry point for the software
  */
 package com.cpb.cs4500 {
-  
+
+  import scala.collection.immutable.ListSet
   import scala.util.parsing.combinator._
+  import com.cpb.cs4500.io._
   import com.cpb.cs4500.parsing._
   import com.cpb.cs4500.rewriting._
-  import com.cpb.cs4500.io._
+  import com.cpb.cs4500.valueGeneration.ValueGenerator
 
   object Runner {
+
+    val minimumTests = 10
 
     def main(args: Array[String]) {
       val input:String = ReadWriter.inputFromFile(args(0))
       val parser = new ADTParser()
-      val rewriter:Rewriter = new Rewriter()
 
       parser.parseAll(parser.spec, input) match {
-        case parser.Success(result, _) => ReadWriter.outputToFile(args(1), rewriter.applyRewriteRules(result) + result.equations)
-        case parser.Failure(msg, _) => println("!!FAILURE!!:\nMalformed Input.\n" + msg) 
-        case parser.Error(msg, _) => println("error, sorry:\n" + msg)
+        case parser.Success(result, _) => generateTerms(result)
+        case parser.Failure(msg, _) => fail(msg)
+        case parser.Error(msg, _) => error(msg)
       }
     }
+
+    def generateTerms(spec: Spec) = {
+      val valGen = new ValueGenerator(spec)
+      val rewriter = new Rewriter(spec)
+      var termList: ListSet[Term] = valGen.generateTerms(minimumTests)
+      var termValuePairs = ListSet[Tuple2[Term, Rhs]]()
+      termList.foreach((term: Term) => termList += (term, rewriter.rewriteTerm(term)))
+    }
+
+    def fail(failureMessage: String) = {
+      println("!!Failure:\nMalformed Input:\n" + failureMessage)
+    }
+
+    def error(errorMessage: String) = {
+      println("error, sorry:\n" + errorMessage)
+    }
+
   }
 }
--- src/main/scala/com/cpb/cs4500/rewriting/Rewriter.scala
+++ src/main/scala/com/cpb/cs4500/rewriting/Rewriter.scala
@@ -1,34 +1,34 @@
 /*
-Rewrites the parsed data into Scheme tests. 
-*/
+ Rewrites the parsed data into Scheme tests.
+ */
+
 package com.cpb.cs4500.rewriting {
-    import com.cpb.cs4500.parsing._
-    import com.cpb.cs4500.valueGeneration._
-    class Rewriter() {
-        def applyRewriteRules(specification:Spec):String  = {
-            "HNNGG"
-        }
+  import com.cpb.cs4500.parsing._
+  import com.cpb.cs4500.valueGeneration._
 
-		def rewrite(values:List[GeneratedValue], rules:List[AnyRef]):List[GeneratedValue] = {
-            //for (value<-values) value match {
-            //    case value:GeneratedPrimitive => rewrite(value, rules)
-            //    case value:GeneratedFunction => {
-            //        List(applyRules(rules, value), 
-            //             rewrite(value.getArguments, rules))
-            //    }
-            //}
-            List()
-		}
-
-        def applyRules(rules:List[AnyRef], value:GeneratedValue):GeneratedValue = {
-            val ruleApplies = true
-            for (rule<-rules) {
-                if (ruleApplies) {
-                    //rewrite the GeneratedValue, probably by making a new one
-                    value
-                }
-            }
-            value
-        }
+  class Rewriter(specification: Spec) {
+    val spec = specification
+    def applyRewriteRules(specification:Spec):String  = {
+      "HNNGG"
     }
+
+    def rewrite(term: Term) = {
+      null
+    }
+
+    def rewrite(values:List[GeneratedValue], rules:List[AnyRef]):List[GeneratedValue] = {
+      List()
+    }
+
+    def applyRules(rules:List[AnyRef], value:GeneratedValue):GeneratedValue = {
+      val ruleApplies = true
+      for (rule<-rules) {
+        if (ruleApplies) {
+          //rewrite the GeneratedValue, probably by making a new one
+          value
+        }
+      }
+      value
+    }
+  }
 }
