45a74c574253e47c651a14bab8c0d8a1556da955
Paul Murray <pawlmurray@gmail.com>
2011-12-05 03:50:48 -0500

    Updated ValueGenerator, got a lot of work done on generateTermArgs

--- src/main/scala/com/cpb/cs4500/valueGeneration/ValueGenerator.scala
+++ src/main/scala/com/cpb/cs4500/valueGeneration/ValueGenerator.scala
@@ -10,15 +10,104 @@ package com.cpb.cs4500.valueGeneration {
     val spec = specification
 
     var generatedTerms = ListSet[Term]()
+	
+	var identToValue:HashMap[String, Terminal] = HashMap[String, Terminal]()
 
+	
     val TypeToConstructer:HashMap[TypeName, List[GeneratedValue]] = HashMap[TypeName, List[GeneratedValue]]()
 
-
-
-    def generateTerms(minTests: Int): ListSet[Term] = {
+    def generateTerms(minTests:Int): ListSet[Term] = {
+	  val allOpSpecs:ListSet[OperationSpec] =spec.getAllOpSpecs()
+	  if (generatedTerms.size < minTests)
+	  {
+		 generateTerms(minTests)
+	  }
       generatedTerms
     }
+	
+	def generateTerm(opSpec:OperationSpec):Term = {
+	    val operationName:String = opSpec.getOpName
+		val returnLiteral:Terminal = opSpec.returnType
+		val arguments:List[Terminal] = opSpec.argTypes.args	
+		var termArgs:Arg = new EmptyArg()
+		for(arg<-arguments)
+		{
+		}
+		new TermID("HNNG")	
+	}
+	
+	def generateTermArgs(arguments:List[Terminal]):Arg =
+	{
+		if(arguments.isEmpty)
+		{
+			new EmptyArg()
+		}
+		else
+		{
+			val arg:Terminal = arguments.head
+			val smallerArguments:List[Terminal] = arguments - arguments.head
+			val nextTermArgs:Arg = generateTermArgs(smallerArguments)
+			var termKey:String = generateRandomMapKey()
+			while (identToValue.contains(termKey))
+			{
+				termKey = generateRandomMapKey()
+			}
+			arg match {
+				case intLit:IntLiteral => 
+				{
+					identToValue.put(termKey, generateRandomIntLiteral())
+					new Args(new TermID(termKey), nextTermArgs)
+				}
+				case boolLit:BooleanLiteral =>
+				{
+					identToValue.put(termKey, generateRandomBooleanLiteral())
+					new Args(new TermID(termKey), nextTermArgs)
+				}
+				case charLit:CharLiteral => 
+				{
+					identToValue.put(termKey, generateRandomCharLiteral())
+					new Args(new TermID(termKey), nextTermArgs)
+				}
+				case stringLit:StringLiteral => 
+				{
+					identToValue.put(termKey, generateRandomStringLiteral())
+					new Args(new TermID(termKey), nextTermArgs)
+				}
+				case typeN:TypeName =>  
+				{
+					new Args(new TermID("HNNNG"), nextTermArgs)	
+				}					
+			}
+		}
+	}
+	
+	
+	
+	
+	def generateRandomIntLiteral():IntLiteral= {
+      IntLiteral(scala.util.Random.nextInt(1001).toString)
+    }
 
+    def generateRandomBooleanLiteral():BooleanLiteral= {
+      BooleanLiteral(scala.util.Random.nextBoolean.toString)
+    }
+
+    def generateRandomCharLiteral():CharLiteral= {
+      CharLiteral(scala.util.Random.nextPrintableChar.toString)
+    }
+
+    def generateRandomStringLiteral():StringLiteral= {
+      val strList = List("string1", "string2", "string3", "string4", "string5")
+      StringLiteral(strList(scala.util.Random.nextInt(5)))
+    }
+	
+	def generateRandomMapKey():String = {
+		scala.util.Random.nextInt(10000000).toString()
+	}
+  }
+}
+
+/*
     def generatedFunctionValues(opspec:OperationSpec) : GeneratedFunction = {
       val operationName:String = opspec.getOpName
       val returnLiteral:Terminal = opspec.returnType
@@ -56,7 +145,7 @@ package com.cpb.cs4500.valueGeneration {
         System.out.println("HNNNG")
       }
       generatedTests
-      /*
+      
        val operationName:String = opspec.getOpName
        val returnLiteral:Terminal = opspec.returnType
        val arguments:List[Terminal] = opspec.argTypes.args
@@ -72,10 +161,8 @@ package com.cpb.cs4500.valueGeneration {
        }
        }
        new GeneratedFunction(operationName, listGenValues, returnLiteral)
-       */
+       
     }
-
-
     def generateRandomIntLiteral():IntLiteral= {
       IntLiteral(scala.util.Random.nextInt(1001).toString)
     }
@@ -94,4 +181,5 @@ package com.cpb.cs4500.valueGeneration {
     }
 
   }
-}
+}*/
+
