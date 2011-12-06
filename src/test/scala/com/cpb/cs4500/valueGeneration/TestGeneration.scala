7259d61ded709bec0fc49f5f4336da77f6ade13e
Paul Murray <pawlmurray@gmail.com>
2011-12-06 04:11:17 -0500

    Fixed

---
+++ src/test/scala/com/cpb/cs4500/valueGeneration/TestGeneration.scala
@@ -0,0 +1,61 @@
+package com.cpb.cs4500.valueGeneration {
+	import org.scalatest.FunSuite
+	import com.cpb.cs4500.parsing._
+	import com.cpb.cs4500.valueGeneration._
+	import scala.collection.mutable.HashMap
+	import scala.collection.immutable.ListSet
+	
+	  
+   class TestGeneration extends FunSuite {
+		val intLit:IntLiteral = new IntLiteral("3")
+		val boolLit:BooleanLiteral = new BooleanLiteral("true")
+		val charLit:CharLiteral = new CharLiteral("#c")
+		val stringLit:StringLiteral = new StringLiteral("HURRDURR")
+		
+		val catType:TypeName = new TypeName("Cat")
+		val dogType:TypeName = new TypeName("Dog")
+		
+		val addOp:Operation = new Operation("addCat")
+		val makeCat:Operation = new Operation("makeCat")
+		val makeCat2:Operation = new Operation("makeCat-2")
+		val makeDog:Operation = new Operation("makeDog")
+		val addBasicTypes:ArgTypes = ArgTypes(List(intLit))
+		val addArgTypes:ArgTypes = ArgTypes(List(catType, boolLit, intLit))
+		val emptyList:List[Terminal] = List[Terminal]()
+		val emptyArgs:ArgTypes = ArgTypes(emptyList)
+		
+		val baseCat:OperationSpec = new OperationSpec(makeCat, addBasicTypes, catType, true)
+		val otherCat:OperationSpec = new OperationSpec(makeCat, addArgTypes, catType, true)
+		val emptyCat:OperationSpec = new OperationSpec(makeCat, emptyArgs, catType, true)
+		val emptyCat2:OperationSpec = new OperationSpec(makeCat2, emptyArgs, catType, true)
+		val notBaseCat:OperationSpec = new OperationSpec(makeDog, addArgTypes, dogType, false)
+		
+		val catADTOperationSpecs:OperationSpecs = OperationSpecs(List(baseCat, otherCat, notBaseCat))
+		val sig:ADTSignature = ADTSignature(catType, catADTOperationSpecs)
+		
+		val sigs:ADTSignatures = ADTSignatures(List(sig))
+		
+		val testSpec:Spec = Spec(sigs, null)
+		
+		val valueGen:ValueGenerator = new ValueGenerator(testSpec)
+		
+		// Correct values
+		val emptyCatExpr:TermExpr = new TermExpr(makeCat, new EmptyArg())
+		
+		var emptyCatList:ListSet[Term] = ListSet[Term]()
+		
+		emptyCatList = emptyCatList + emptyCatExpr
+
+		valueGen.createBasicCreatorMap()
+		
+		test("testCreateTests1") {
+			expect("5") { valueGen.createTestsForOpSpec(baseCat, 1) }
+		}
+		test("testCreateTests2") {
+			expect("5") { valueGen.createTestsForOpSpec(otherCat, 1) }
+		}
+		test("testCreateTests3") {
+			expect(emptyCatList) { valueGen.createTestsForOpSpec(emptyCat, 1) }
+		}
+   }
+}
