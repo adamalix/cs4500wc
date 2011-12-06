63401ea206a1f66dbf94f9fbcba1ce968ebe5041
Paul Murray <pawlmurray@gmail.com>
2011-12-06 12:21:04 -0500

    Updated Rewrite rules and added some functionality to Spec

--- src/main/scala/com/cpb/cs4500/rewriting/Rewriter.scala
+++ src/main/scala/com/cpb/cs4500/rewriting/Rewriter.scala
@@ -8,31 +8,90 @@ package com.cpb.cs4500.rewriting {
 
   class Rewriter(specification: Spec) {
     val spec = specification
-
-    def rewrite(terms:List[Term]):List[Term] = {
+    val termIdMap = Map[TermID, TypeLiteral]()
+    
+    def rewrite(terms:List[Term]):List[Rhs] = {
         val eqs = this.spec.equations.eqs
-        for (term<-terms) {
-            for (eq<-eqs) {
-                if (ruleApplies(term, eq)) {
-                    //rule applies: rewrite term; break the loop
-                    rewriteTerm(term, eq)
-                }
-            }
-        }
-        List()
+        var rewrittenTerms = List[Rhs]()
+        for (term<-terms) 
+		{
+            rewrittenTerms = rewrittenTerms :+ rewriteTerm(term)
+        }  
+        rewrittenTerms
     }
 
     //this function is called under the pretense that the rule applies to the term
-    def rewriteTerm(term:Term, eq:Equation):Term = {
-        term
-    }
-
-    def ruleApplies(term:Term, eq:Equation):Boolean = {
+    def rewriteTerm(term:Term): Rhs = {
         term match {
-            case term:TermID => false
-            //potential ambiguity if term on left is an ID instead of an Expr 
-            //case term:TermExpr => term.op.equals(eq.left.op)
+            case termid: TermID => {
+                new RhsID(termIdMap(termid).toString())
+            }
         }
     }
+        
+/*        for (eq<-eqs) 
+			{
+               term match {
+                    case termid: TermID => {
+                        new RH
+                    }
+                    case termexpr: TermExpr => {
+                    }
+               }
+               // if (ruleApplies(term, eq)) {
+                    //rule applies: rewrite term; break the loop
+                //    rewriteTerm(term, eq)
+               // }
+            }
+        term*/
+    
+
+    def ruleApplies(term: TermExpr, eq: Equation): Boolean = {
+		val eqLeft:Term = eq.left
+		eqLeft match {
+			case leftIdent: TermID => false
+			case leftExpr: TermExpr => term.op.equals(leftExpr.op) && term.args.length().equals(leftExpr.args.length())
+        }
+    }
+
+/*    
+    def checkIfArgsEqual(termArgs: Arg, patternArgs: Arg): Boolean = {
+        if (termArgs.isEmpty() && patternArgs.isEmpty()) 
+            true
+        termArgs match {
+            case emptyTermArgs: EmptyArg => false
+            case termArguments: Args => {
+                patternArgs match {
+                    case emptyPatternArgs: EmptyArg => false
+                    case patternArguments: Args => {
+                         termEqual(termArguments.term, patternArguments.term) &&
+                         checkIfArgsEqual(termArguments.args, patternArguments.args)
+                    }
+                }
+            }
+        }
+    }
+    
+    def termEqual(t1: Term, t2: Term): Boolean = {
+        t1 match {
+            case t1Ident:TermID => {
+                t2 match {
+                    case t2Ident:TermID => true
+                    case t2Expr:TermExpr => false
+                }
+            }
+            case t1Expr:TermExpr => {
+                t2 match {
+                    case t2Ident:TermID => false
+                    case t2Expr:TermExpr => true
+                }
+            }
+        }
+    }
+    
+    def termExprEqual(t1: TermExpr, t2: TermExpr): Boolean = {
+        t1.op.equals(t2.op) && t1.args.length().equals(t2.args.length())
+    }
+    */
   }
 }
